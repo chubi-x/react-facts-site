e08d623311a2ee490b6194fadbced0b5e33edc02
chubi-x <chubixax@protonmail.com>
2022-08-10 15:20:02 +0100

    chore: specify fonts

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="nav-container">
+        <div className="logo-header">
+          <img src="" alt="" />
+          <h1>React Facts</h1>
+        </div>
+        <div className="dark-mode-toggle"></div>
+      </div>
+    </nav>
+  );
+}
